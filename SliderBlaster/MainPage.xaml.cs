ffc34c0875ac03e0b703b2cd6b72885953efd087
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-07 20:06:54 -0400

    first Commit

---
+++ SliderBlaster/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SliderBlaster
+{
+    public partial class MainPage : ContentPage
+    {
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+
+            label_roda.Rotation = slider_roda.Value;
+
+            int labelValor = Convert.ToInt32(slider_roda.Value);
+
+            label_valorDoSlider.Text = labelValor.ToString();
+
+        }
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
